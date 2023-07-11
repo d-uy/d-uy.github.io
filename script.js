e70b30493b57d924cd2db4f69059edc74853ca90
d-uy <136248381+d-uy@users.noreply.github.com>
2023-07-11 21:33:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ console.log("这是一个JavaScript示例");
 function changeColor() {
     document.body.style.backgroundColor = "pink";
 }
+<script>
+  var image = document.getElementById("myImage");
+  var text = document.getElementById("myText");
+
+  image.onclick = function() {
+    if (text.style.display === "none") {
+      text.style.display = "inline";
+    } else {
+      text.style.display = "none";
+    }
+  };
+</script>
+ 
