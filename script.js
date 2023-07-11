3c33df0b613fe6b297a0b215e2348def63e98a9f
d-uy <136248381+d-uy@users.noreply.github.com>
2023-07-11 21:51:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ console.log("这是一个JavaScript示例");
 function changeColor() {
     document.body.style.backgroundColor = "pink";
 }
+
 function replaceWithText() {
     var image = document.getElementById("myImage");
-    image.src = "path/to/text.png";
-    image.alt = "一句话";
-  }
+    var text = document.getElementById("myText");
+    var sentence = "博，快含住";
+
+    image.style.display = "none";  // 隐藏图片
+    text.innerText = sentence;  // 设置句子内容
+}
 
